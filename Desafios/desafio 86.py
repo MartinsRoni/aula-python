17da6d64cb81e08dfa6a7cbc899dfc762a476400
MartinsRoni <martinsroni03@gmail.com>
2024-12-11 21:28:06 -0300

    Challenge 87 Done.

--- Desafios/desafio 86.py
+++ Desafios/desafio 86.py
@@ -1,6 +1,5 @@
 # Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
 # No final mostre a matriz com a formatação correta.
-
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
     for c in range(0,3):
@@ -9,4 +8,3 @@ print('=-' *30)
 for l in range(0,3):
     for c in range(0,3):
         print(f'[{matriz[l][c]:^5}]', end='')
-    print()
--- Desafios/desafio 87.py
+++ Desafios/desafio 87.py
@@ -1,1 +1,26 @@
-# Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.   
+# Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]   
+spar = scol = mai = 0
+
+for l in range(0,3):# As três primeiras linhas estão recebendo os valores para as matrizes
+    for c in range(0,3):
+        matriz[l][c] = int(input('Digite um valor para matriz:'))
+print('=-'*30)
+for l in range(0,3):# As três ultimas linhas estão estruturando a forma a ser exibida
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] %2 == 0:    
+            spar += matriz[l][c]  
+    print()
+print('=-'*30)
+print(f'A Soma de todos os valores pares digitados é de :{spar}')
+for l in range(0,3):# Somando apenas a terceira coluna  
+    scol += matriz[l][2]
+print(f'A soma de todos os valores da terceira coluna é de :{scol}')
+for  c in range(0,3):# Verificando o maior numero da segunda linha
+    if c == 0:
+        mai = matriz[1][c]
+    elif matriz[1][c]> mai:
+        mai = matriz[1][c]
+print(f'E o maior valore da segunda coluna é de :{mai}')
